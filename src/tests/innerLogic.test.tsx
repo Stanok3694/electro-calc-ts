2ede4668cf4df329698126de5995a6acae3646c3
Stanislav Okunev <stanok3694@yandex.ru>
2017-08-28 11:31:41 +0300

    Update innerLogic.test.tsx

--- src/tests/innerLogic.test.tsx
+++ src/tests/innerLogic.test.tsx
@@ -7,31 +7,26 @@ import SocialNorms from '../logic/socialNorms';
 
 describe('electricity payments inner functional OVER DAY NORM calculation', () => {
 	let thisMonthDelta: number = findDelta(2414, 2342);
-
 	it('should return correct delta btw months', () => {
 		expect(thisMonthDelta).toBe(72);  
 	});
 
 	let thisMonthOverNormDelta: number = findDelta(thisMonthDelta, SocialNorms.day);
-
 	it("should return correct delta btw month's delta and social norm", () => {
 		expect(thisMonthOverNormDelta).toBe(15);
 	});
 	
 	let constSocialNormDayPay: number = findPayment(SocialNorms.day, Tarifs.dayUnderNorm);
-
 	it('should calculate const social norm day value correct', () => {
 		expect(constSocialNormDayPay).toBe(204.06);
 	});
-
+	
 	let thisMonthOverDayNormDeltaPay: number = findPayment(thisMonthOverNormDelta, Tarifs.dayOverNorm);
-
 	it('should calculate over social norm day value correct', () => {
 		expect(thisMonthOverDayNormDeltaPay).toBe(103.20);
 	});
 
 	let thisMonthDayFinalPay: number = findResult(thisMonthOverDayNormDeltaPay, constSocialNormDayPay);
-
 	it('should return correct final day payment result', () => {
 		expect(thisMonthDayFinalPay).toBe(307.26);
 	});
