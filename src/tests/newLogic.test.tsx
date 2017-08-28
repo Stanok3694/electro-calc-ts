d3753a358839d3651daec099be370a4444cc1fd3
Stanislav Okunev <stanok3694@yandex.ru>
2017-08-28 11:33:45 +0300

    minor code-style changes in newLogic.test

--- src/tests/newLogic.test.tsx
+++ src/tests/newLogic.test.tsx
@@ -2,7 +2,6 @@ import findDelta from '../logic/findDelta';
 import findPayment from '../logic/findPayment';
 import findMonthResult from '../logic/findMonthResult';
 import calculate from '../logic/calculate';
-
 import Month from '../logic/Month';
 import TopValueObject from '../logic/TopValueObject';
 import Tarifs from '../logic/tarifs';
@@ -11,13 +10,11 @@ import SocialNorms from '../logic/socialNorms';
 
 describe('Day over norm case: ', () => {
 	const constSocialNormDayPay: number = findPayment(SocialNorms.day, Tarifs.dayUnderNorm);
-	
 	const topDayValueObject = new TopValueObject(2414, 2342);
 	const dayMonthDelta = findDelta(topDayValueObject.currentMonthTopValue, topDayValueObject.previousMonthTopValue);
 
 	let thisMonthOverDayNormDeltaPay: number = findDelta(dayMonthDelta, SocialNorms.day);
 	let dayPayResult = calculate(constSocialNormDayPay, thisMonthOverDayNormDeltaPay).overDayNorm;
-	
 	it('should calculate day phase correct <- 307.26', () => {
 		expect(dayPayResult).toBe(307.26);
 	});
@@ -25,13 +22,11 @@ describe('Day over norm case: ', () => {
 
 describe('Night over norm case: ', () => {
 	const constSocialNormNightPay: number = findPayment(SocialNorms.night, Tarifs.nightUnderNorm);
-	
 	const topNightValueObject = new TopValueObject(1660, 1625);
 	const nightMonthDelta = findDelta(topNightValueObject.currentMonthTopValue, topNightValueObject.previousMonthTopValue);
 
 	let thisMonthOverNightNormDeltaPay: number = findDelta(nightMonthDelta, SocialNorms.night);
 	let nightPayResult = calculate(constSocialNormNightPay, thisMonthOverNightNormDeltaPay).overNightNorm;
-	
 	it('should calculate night phase correct <- 74.13', () => {
 		expect(nightPayResult).toBe(74.13);
 	});
@@ -42,7 +37,6 @@ describe('Full result over norm: ', () => {
 	const jule = new Month(2414, 1660);
 
 	let resultPayment: number = findMonthResult(jule, june);
-	
 	it('should calculate all cycle correct <- 381.39', () => {
 		expect(resultPayment).toBe(381.39);
 	});
