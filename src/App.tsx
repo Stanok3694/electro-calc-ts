fd22413b89137551b298b8bf1178551340c91292
Stanok3694 <stanok3694@yandex.ru>
2017-09-01 00:22:56 +0300

    add react-router + test it + remove old components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,46 @@
 import * as React from 'react';
 import './App.css';
 
-import TopValueComponent from './components/Top-value';
+import {
+  BrowserRouter as Router,
+  Route as Route,
+  Link as Link
+} from 'react-router-dom';
+
+const BasicAppLayer = () => (
+  <Router>
+    <div>
+      <ul>
+        <li><Link to="/">Menu</Link></li>
+        <li><Link to="/about">Electricity Payments</Link></li>
+        <li><Link to="/topics">Deposit Profit</Link></li>
+      </ul>
+
+      <hr/>
+
+      <Route exact path="/" component={Home}/>
+      <Route path="/about" component={About}/>
+      </div>
+  </Router>
+);
+
+const Home = () => (
+  <div>
+    <h2>Home</h2>
+  </div>
+);
+
+const About = () => (
+  <div>
+    <h2>About</h2>
+  </div>
+);
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <TopValueComponent type="day" />
-        <TopValueComponent type="night" />
+        <BasicAppLayer />
       </div>
     );
   }
